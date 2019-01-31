7c205d4530f8f2937f97ed19ca658220128f1117
liu_lei <liu_lei_programmer@163.com>
2019-01-31 15:00:51 +0800

    新建工程，测试仓库关联情况

---
+++ dubbo-client/src/main/java/com/liulei/App.java
@@ -0,0 +1,13 @@
+package com.liulei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
